218abf5d5abe7d3b3a1834d6485736eca748ba90
Diego Borigen <diego@iddigital.com.au>
2025-08-02 18:19:37 +0800

    Fix variable issue

--- api/getFeed.tsx
+++ api/getFeed.tsx
@@ -1,16 +1,15 @@
 export default async function getFeed() {
-    const token = process.env.INSTAGRAM_TOKEN;
-  
+    const token = process.env.NEXT_PUBLIC_INSTAGRAM_TOKEN;
+
     const res = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type,thumbnail_url&limit=6&access_token=${token}`);
-  
+
     const resJson:any = await res.json();
-  
+
     const result = resJson.data;
-  
+
     if (!res.ok) {
         throw new Error('Failed to fetch data')
     }
-  
+
     return result;
   }
-  
