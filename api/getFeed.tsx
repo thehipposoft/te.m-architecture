def1438629679738628b2ad5d187d3606826cc96
Diego Borigen <diego@iddigital.com.au>
2025-10-20 21:40:41 +0800

    Refactor Insgragram API

--- api/getFeed.tsx
+++ api/getFeed.tsx
@@ -1,26 +1,14 @@
 export default async function getFeed() {
-    const longLivedAccessToken = process.env.INSTAGRAM_TOKEN;
-    const apiUrl = `https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${longLivedAccessToken}`;
+    const response = await fetch(`https://wp.thehipposoft.com/wp-json/hippo/v1/token?website=temarqui`);
+    const data = await response.json();
+    const token = data.token;
 
-    const refreshAccessToken = async () => {
-        try {
-            const response = await fetch(apiUrl);
-            const data = await response.json();
+    const res = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type,thumbnail_url&limit=6&access_token=${token}`);
+    const resJson = await res.json();
 
-            const res = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type,thumbnail_url&limit=6&access_token=${data.access_token}`);
-            const resJson:any = await res.json();
-            const result = resJson.data;
+    if (!res.ok) {
+        throw new Error('Failed to fetch Instagram feed');
+    }
 
-            if (!res.ok) {
-                throw new Error('Failed to fetch data')
-            }
-
-            return result;
-
-        } catch (error) {
-            throw new Error('Failed to refresh token');
-        }
-    };
-
-    return refreshAccessToken();
+    return resJson.data;
   }
